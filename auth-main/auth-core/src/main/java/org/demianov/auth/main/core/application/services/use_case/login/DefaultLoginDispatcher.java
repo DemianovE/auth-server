b3bf8d430aad5a3e87483453b06472d490410358
intermaps-egor <87388931+DemianovE@users.noreply.github.com>
2026-04-13 12:33:39 +0200

    fix(core): update DefaultLoginDispatcher to handle loginResult and user input.
    Dispatcher role was changed to result change, handlers and actual result generation was moved outside.
    
    The Default implementation does nothing, so @Internal annotation is used

--- auth-main/auth-core/src/main/java/org/demianov/auth/main/core/application/services/use_case/login/DefaultLoginDispatcher.java
+++ auth-main/auth-core/src/main/java/org/demianov/auth/main/core/application/services/use_case/login/DefaultLoginDispatcher.java
@@ -1,46 +1,27 @@
 package org.demianov.auth.main.core.application.services.use_case.login;
 
 import org.demianov.auth.main.core.application.models.LoginResult;
-import org.demianov.auth.main.core.application.ports.out.security.TokenGeneratorPort;
 import org.demianov.auth.main.core.application.ports.in.login.LoginDispatcher;
-import org.demianov.auth.main.core.application.ports.in.login.LoginHandler;
 import org.demianov.auth.main.core.domain.models.User;
-import org.demianov.auth.main.core.exceptions.HandlerException;
+import org.demianov.auth.main.kernel.application.annotations.Internal;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-
+/**
+ * This implementation does nothing.
+ * {@inheritDoc}
+ */
+@Internal
 public final class DefaultLoginDispatcher implements LoginDispatcher {
-    /** The port to work with the token generation. */
-    private final TokenGeneratorPort tokenGenerator;
-    /** The list of handlers to work with the login process. */
-    private final List<LoginHandler> handlers;
 
     /**
-     * Canonical constructor. Perform mandatory tokenGenerator validation.
-     * @param tokenGeneratorParam the port to work with the token generation
-     * @param handlersParam the list of handlers to work with the login process
+     * Canonical constructor.
      */
-    public DefaultLoginDispatcher(
-            final TokenGeneratorPort tokenGeneratorParam,
-            final List<LoginHandler> handlersParam) {
-        this.tokenGenerator = Objects.requireNonNull(tokenGeneratorParam,
-                "tokenGenerator");
-        this.handlers = (handlersParam != null)
-                ? List.copyOf(handlersParam)
-                : Collections.emptyList();
+    public DefaultLoginDispatcher() {
     }
 
     @Override
-    public LoginResult dispatch(final User user) {
-        try {
-            this.handlers.forEach(handler ->
-                    handler.handle(user));
-        } catch (HandlerException e) {
-            return new LoginResult.Failure(e);
-        }
-
-        return this.tokenGenerator.generate(user);
+    public LoginResult dispatch(
+            final LoginResult loginResult,
+            final User user) {
+        return loginResult;
     }
 }

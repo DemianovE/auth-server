45855bd004803d783afff9aa6f740b6d116f05d8
intermaps-egor <87388931+DemianovE@users.noreply.github.com>
2026-04-13 12:36:00 +0200

    fix(core): update LoginUseCase. See description.
    The extensive change of the LoginUseCase.
    1) adoption of new handlers/dispatcher/listeners logic;
    2) adoption of priority handling;
    3) handlers/listeners centralized logic;
    4) central failure handling;
    5) add explicit usage of FailureStrategy;

--- auth-main/auth-core/src/main/java/org/demianov/auth/main/core/application/services/use_case/login/LoginUseCase.java
+++ auth-main/auth-core/src/main/java/org/demianov/auth/main/core/application/services/use_case/login/LoginUseCase.java
@@ -1,7 +1,13 @@
 package org.demianov.auth.main.core.application.services.use_case.login;
 
+import org.demianov.auth.main.core.application.models.FailureStrategy;
+import org.demianov.auth.main.core.application.models.TokenPair;
+import org.demianov.auth.main.core.application.ports.in.login.CriticalLoginHandler;
 import org.demianov.auth.main.core.application.ports.in.login.LoginDispatcher;
-import org.demianov.auth.main.kernel.domain.models.Email;
+import org.demianov.auth.main.core.application.ports.out.listeners.LoginListener;
+import org.demianov.auth.main.core.application.ports.out.security.TokenGeneratorPort;
+import org.demianov.auth.main.core.exceptions.ports.UnexpectedSystemException;
+import org.demianov.auth.main.kernel.api.Prioritized;
 
 import org.demianov.auth.main.core.application.models.LoginResult;
 import org.demianov.auth.main.core.application.ports.in.login.LoginInputPort;
@@ -15,7 +21,9 @@ import org.demianov.auth.main.core.exceptions.AuthCoreException;
 import org.demianov.auth.main.core.exceptions.models.user.UserNotFoundException;
 import org.demianov.auth.main.core.exceptions.tags.LoginPortExceptions;
 
-import java.util.Objects;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
 
 public final class LoginUseCase implements LoginInputPort {
     /** The user-repository. Used to work with the user persistence layer. */
@@ -23,46 +31,132 @@ public final class LoginUseCase implements LoginInputPort {
     /** The password hasher. Used to hash the user's password. */
     private final PasswordHasherPort passwordHasher;
     /** The login dispatcher. Used to dispatch the login process
-     * to the appropriate handler.
-     */
+     * to the appropriate handler.*/
     private final LoginDispatcher dispatcher;
+    /** The token generator. Used to generate the user's tokens. */
+    private final TokenGeneratorPort tokenGenerator;
+    /** The critical handlers. Used to perform critical actions */
+    private final List<CriticalLoginHandler> criticalHandlers;
+    /** The login listeners. Used to perform actions after the login process. */
+    private final List<LoginListener> listeners;
+    /** The failure strategy. By default, is
+     * {@link FailureStrategy#CONTINUE_ON_FAIL} */
+    private final FailureStrategy failureStrategy;
 
-    /**
-     * Canonical constructor. Perform mandatory field validation.
-     * @param userRepoParam user repository.
-     * @param passwordHasherParam password hasher.
-     * @param dispatcherParam login dispatcher.
-     */
-    public LoginUseCase(
-            final UserRepoPort userRepoParam,
-            final PasswordHasherPort passwordHasherParam,
-            final LoginDispatcher dispatcherParam) {
-        this.userRepo = Objects.requireNonNull(userRepoParam,
-                "userRepo cannot be null");
-        this.passwordHasher = Objects.requireNonNull(passwordHasherParam,
-                "passwordHasher cannot be null");
-        this.dispatcher = Objects.requireNonNull(dispatcherParam,
-                "dispatcher cannot be null");
+    LoginUseCase(final UserRepoPort userRepoParam,
+                 final PasswordHasherPort passwordHasherParam,
+                 final LoginDispatcher dispatcherParam,
+                 final TokenGeneratorPort tokenGeneratorParam,
+                 final List<CriticalLoginHandler> criticalHandlersParam,
+                 final List<LoginListener> listenersParam,
+                 final FailureStrategy failureStrategyParam) {
+        this.userRepo = userRepoParam;
+        this.passwordHasher = passwordHasherParam;
+        this.dispatcher = dispatcherParam;
+        this.tokenGenerator = tokenGeneratorParam;
+        this.criticalHandlers = sortByPriority(criticalHandlersParam);
+        this.listeners = sortByPriority(listenersParam);
+        this.failureStrategy = failureStrategyParam;
     }
 
     @Override
     public LoginResult execute(final LoginCommand request) {
         try {
-            User user = this.userRepo.findByEmail(
-                    new Email(request.email().value()))
+            User user = this.userRepo.findByEmail(request.email())
                     .orElseThrow(() ->
                             new UserNotFoundException(
                                     request.email().value()));
+
+            executeHandlers(this.criticalHandlers,
+                    handler -> handler.perform(user));
+
             user.authenticate(request.password(), this.passwordHasher);
 
             this.userRepo.save(user);
 
-            return this.dispatcher.dispatch(user);
+            TokenPair generatedPair = this.tokenGenerator.generate(user);
+            LoginResult result = new LoginResult.Success(
+                    generatedPair.accessToken(),
+                    generatedPair.refreshToken(),
+                    generatedPair.expiresIn(),
+                    generatedPair.refreshTokenTtl()
+            );
+
+            executeHandlers(this.listeners,
+                    listener -> listener.onSuccess(user));
+            return this.dispatcher.dispatch(result, user);
         } catch (AuthCoreException e) {
-            if (e instanceof LoginPortExceptions tagged) {
-                return new LoginResult.Failure(tagged);
-            }
-            throw e;
+            return handleFailure(e, request);
         }
+    }
+
+    /**
+     * Perform the failure logic.
+     * @param e the exception.
+     * @param request the login command request payload.
+     * @return the login result.
+     * @implNote The failure listeners if failed will be ignored. As such
+     * the original exception will be <b>always</b> thrown
+     */
+    private LoginResult handleFailure(
+            final AuthCoreException e,
+            final LoginCommand request) {
+        try {
+            executeHandlers(this.listeners,
+                    listener -> listener.onFailure(request));
+        } catch (Exception ignored) {
+        }
+
+        return switch (e) {
+            case LoginPortExceptions tagged -> new LoginResult.Failure(tagged);
+
+            default -> new LoginResult.Failure(
+                    new UnexpectedSystemException(e));
+        };
+    }
+
+    /**
+     * Perform handler logic. If the failure strategy is FAIL_FAST,
+     * then the exception is thrown.
+     * @param handlers list of handlers.
+     * @param action action to perform.
+     * @param <T> type of the handlers.
+     */
+    private <T> void executeHandlers(
+            final List<T> handlers,
+            final Consumer<T> action) {
+        for (T handler : handlers) {
+            try {
+                action.accept(handler);
+            } catch (AuthCoreException e) {
+                if (this.failureStrategy == FailureStrategy.FAIL_FAST
+                        || handler instanceof CriticalLoginHandler) {
+                    throw e;
+                }
+            } catch (Exception e) {
+                if (this.failureStrategy == FailureStrategy.FAIL_FAST
+                        || handler instanceof CriticalLoginHandler) {
+                    throw new UnexpectedSystemException(e);
+                }
+            }
+        }
+    }
+
+    /**
+     * Perform Priotiy sorting on a list of handlers.
+     * @param listOfValues list of handlers.
+     * @return sorted the list of handlers.
+     * @param <T> type of the handlers.
+     */
+    private <T extends Prioritized> List<T> sortByPriority(
+            final List<T> listOfValues) {
+        if (listOfValues == null) {
+            return List.of();
+        }
+
+        return listOfValues.stream()
+                .sorted(Comparator.comparingInt(
+                        Prioritized::getPriority).reversed())
+                .toList();
     }
 }

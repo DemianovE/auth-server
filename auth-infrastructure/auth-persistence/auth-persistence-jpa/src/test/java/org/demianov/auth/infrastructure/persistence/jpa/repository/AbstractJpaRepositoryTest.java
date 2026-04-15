f63fecaf3aff79fa5383922b6383f37aebbfa31d
intermaps-egor <87388931+DemianovE@users.noreply.github.com>
2026-04-16 00:57:11 +0200

    fix(auth-persistence-jpa): correct TestEntity used for testing of AbstractJpaRepositoryTest

--- auth-infrastructure/auth-persistence/auth-persistence-jpa/src/test/java/org/demianov/auth/infrastructure/persistence/jpa/repository/AbstractJpaRepositoryTest.java
+++ auth-infrastructure/auth-persistence/auth-persistence-jpa/src/test/java/org/demianov/auth/infrastructure/persistence/jpa/repository/AbstractJpaRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.demianov.auth.infrastructure.persistence.jpa.repository;
 
+import org.demianov.auth.infrastructure.persistence.jpa.entities.AbstractAuditEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AbstractJpaRepositoryTest {
 
-    private static class TestEntity {}
+    private static class TestEntity extends AbstractAuditEntity {}
     private static class TestRepo extends AbstractJpaRepository<TestEntity, UUID> {
         public TestRepo() {
             super(TestEntity.class);

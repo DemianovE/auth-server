99a669301c849c73ba9a92ef8320eb0de92e6639
intermaps-egor <87388931+DemianovE@users.noreply.github.com>
2026-04-16 01:16:23 +0200

    fix(auth-persistence-jpa): correct AbstractJpaRepositoryTest

--- auth-infrastructure/auth-persistence/auth-persistence-jpa/src/test/java/org/demianov/auth/infrastructure/persistence/jpa/repository/AbstractJpaRepositoryTest.java
+++ auth-infrastructure/auth-persistence/auth-persistence-jpa/src/test/java/org/demianov/auth/infrastructure/persistence/jpa/repository/AbstractJpaRepositoryTest.java
@@ -1,12 +1,18 @@
 package org.demianov.auth.infrastructure.persistence.jpa.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceException;
 import org.demianov.auth.infrastructure.persistence.jpa.entities.AbstractAuditEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class AbstractJpaRepositoryTest {
 
@@ -26,5 +32,42 @@ public class AbstractJpaRepositoryTest {
     void constructor_Success() {
         TestRepo repo = new TestRepo();
         assertEquals(TestEntity.class, repo.testGetEntityClass());
+    }
+
+    @Test
+    @DisplayName("Should ignore exception when entity to delete does not exist")
+    void delete_ShouldIgnorePersistenceException() {
+        TestRepo repo = new TestRepo();
+        EntityManager emMock = mock(EntityManager.class);
+
+        ReflectionTestUtils.setField(repo, "entityManager", emMock);
+
+        UUID randomId = UUID.randomUUID();
+        when(emMock.getReference(any(), any()))
+                .thenThrow(new PersistenceException("Simulated failure"));
+
+        assertDoesNotThrow(() -> repo.delete(randomId));
+
+        verify(emMock).getReference(eq(TestEntity.class), eq(randomId));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when not PersistenceException")
+    void delete_Failure() {
+        String message = "Simulated failure";
+        TestRepo repo = new TestRepo();
+        EntityManager emMock = mock(EntityManager.class);
+
+        ReflectionTestUtils.setField(repo, "entityManager", emMock);
+
+        UUID randomId = UUID.randomUUID();
+        when(emMock.getReference(any(), any()))
+                .thenThrow(new IncompatibleClassChangeError(message));
+
+        assertThatThrownBy(() -> repo.delete(randomId))
+                .isInstanceOf(IncompatibleClassChangeError.class)
+                .hasMessage(message);
+
+        verify(emMock).getReference(eq(TestEntity.class), eq(randomId));
     }
 }
